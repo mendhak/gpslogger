151f499cb06e8ec31b6a97ac4e37ced028bd5770
Mendhak <smaltaf@msn.com>
2012-05-06 22:49:04 +0100

    Reformatted code file

--- GPSLogger/src/com/mendhak/gpslogger/loggers/PlainTextFileLogger.java
+++ GPSLogger/src/com/mendhak/gpslogger/loggers/PlainTextFileLogger.java
@@ -16,59 +16,67 @@ import com.mendhak.gpslogger.loggers.IFileLogger;
  *
  * @author Jeroen van Wilgenburg
  */
-public class PlainTextFileLogger implements IFileLogger {
+public class PlainTextFileLogger implements IFileLogger
+{
 
-	private File file;
-	private boolean useSatelliteTime;
+    private File file;
+    private boolean useSatelliteTime;
 
-	public PlainTextFileLogger(File file, boolean useSatelliteTime) {
-		this.file = file;
-		this.useSatelliteTime = useSatelliteTime;
-	}
+    public PlainTextFileLogger(File file, boolean useSatelliteTime)
+    {
+        this.file = file;
+        this.useSatelliteTime = useSatelliteTime;
+    }
 
-	@Override
-	public void Write(Location loc) throws Exception {
-		if (!file.exists()) {
-			file.createNewFile();
+    @Override
+    public void Write(Location loc) throws Exception
+    {
+        if (!file.exists())
+        {
+            file.createNewFile();
 
-			FileOutputStream writer = new FileOutputStream(file, true);
-			BufferedOutputStream output = new BufferedOutputStream(writer);
-			String header = "time,lat,lon,elevation,accuracy,bearing,speed\n";
-			output.write(header.getBytes());
-			output.flush();
-			output.close();
-		}
+            FileOutputStream writer = new FileOutputStream(file, true);
+            BufferedOutputStream output = new BufferedOutputStream(writer);
+            String header = "time,lat,lon,elevation,accuracy,bearing,speed\n";
+            output.write(header.getBytes());
+            output.flush();
+            output.close();
+        }
 
-		FileOutputStream writer = new FileOutputStream(file, true);
-		BufferedOutputStream output = new BufferedOutputStream(writer);
+        FileOutputStream writer = new FileOutputStream(file, true);
+        BufferedOutputStream output = new BufferedOutputStream(writer);
 
-		Date now;
+        Date now;
 
-		if (useSatelliteTime) {
-			now = new Date(loc.getTime());
-		} else {
-			now = new Date();
-		}
+        if (useSatelliteTime)
+        {
+            now = new Date(loc.getTime());
+        }
+        else
+        {
+            now = new Date();
+        }
 
-		String dateTimeString = Utilities.GetIsoDateTime(now);
+        String dateTimeString = Utilities.GetIsoDateTime(now);
 
-		String outputString = String.format("%s,%f,%f,%f,%f,%f,%f\n", dateTimeString,
-									loc.getLatitude(),
-									loc.getLongitude(),
-									loc.getAltitude(),
-									loc.getAccuracy(),
-									loc.getBearing(),
-									loc.getSpeed());
+        String outputString = String.format("%s,%f,%f,%f,%f,%f,%f\n", dateTimeString,
+                loc.getLatitude(),
+                loc.getLongitude(),
+                loc.getAltitude(),
+                loc.getAccuracy(),
+                loc.getBearing(),
+                loc.getSpeed());
 
-		output.write(outputString.getBytes());
-		output.flush();
-		output.close();
-	}
+        output.write(outputString.getBytes());
+        output.flush();
+        output.close();
+    }
 
-	@Override
-	public void Annotate(String description, Location loc) throws Exception {
-	    // TODO Auto-generated method stub
+    @Override
+    public void Annotate(String description, Location loc) throws Exception
+    {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
 }
