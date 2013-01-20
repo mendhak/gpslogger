d61b3dae69a5a91c1303c676b414bc850aefa415
Mendhak <smaltaf@msn.com>
2013-01-20 20:56:29 +0000

    Skeleton code with basic FTP(S) functionality

--- GPSLogger/src/com/mendhak/gpslogger/senders/ftp/FtpHelper.java
+++ GPSLogger/src/com/mendhak/gpslogger/senders/ftp/FtpHelper.java
@@ -18,9 +18,16 @@
 
 package com.mendhak.gpslogger.senders.ftp;
 
+import android.util.Log;
 import com.mendhak.gpslogger.senders.IFileSender;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPSClient;
 
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class FtpHelper implements IFileSender
@@ -36,5 +43,95 @@ public class FtpHelper implements IFileSender
     public boolean accept(File file, String s)
     {
         return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
+
+class TestFtpHandler implements Runnable
+{
+
+    @Override
+    public void run()
+    {
+
+        boolean useFtps = false;
+        FTPClient client = null;
+        String protocol = "SSL";
+
+        //If implicit = true, set port to 990. If false, set to 21.  If useFtps = false, set to 21.
+        boolean implicit = true;
+        int port = 990;
+
+        try
+        {
+            Log.v("FTPTEST", "Connecting...");
+
+            if(useFtps)
+            {
+                client = new FTPSClient(protocol, implicit);
+
+                KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+                kmf.init(null, null);
+                KeyManager km = kmf.getKeyManagers()[0];
+                ((FTPSClient) client).setKeyManager(km);
+
+            }
+            else
+            {
+                client = new FTPClient();
+            }
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+
+        try {
+            client.connect("ftp.secureftp-test.com", port);
+
+            //
+            // When login success the login method returns true.
+            //
+            if (client.login("test", "test"))
+            {
+                Log.v("FTPTEST", "logged in");
+                client.enterLocalPassiveMode();
+                String data = "test data";
+                ByteArrayInputStream in = new ByteArrayInputStream(data.getBytes());
+//                boolean result = client.storeFile("test2.txt", in);
+//                in.close();
+//                if (result)
+//                {
+//                    Log.v("FTPTEST", "succeeded in writing file");
+//                }
+//                else
+//                {
+//                    Log.v("FTPTEST", "failed to write file");
+//                }
+
+            }
+            else
+            {
+                Log.e("FTPTEST", "Login fail...");
+            }
+
+        } catch (IOException e) {
+            Log.e("FTPTEST", e.getMessage());
+            e.printStackTrace();
+        } finally {
+            try {
+                //
+                // Closes the connection to the FTP server
+                //
+                Log.v("FTPTEST", "Logging out...");
+                client.logout();
+                Log.v("FTPTEST", "Disconnecting...");
+                client.disconnect();
+            } catch (IOException e) {
+                Log.e("FTPTEST", e.getMessage());
+                e.printStackTrace();
+            }
+        }
     }
 }
