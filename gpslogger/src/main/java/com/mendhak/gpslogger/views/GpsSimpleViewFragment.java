c75018d4b856bc589a69df3b36ae34b9458f1f01
mendhak <smaltaf@msn.com>
2014-04-17 06:42:36 +0100

    Hyphens for points/distance icons

--- gpslogger/src/main/java/com/mendhak/gpslogger/views/GpsSimpleViewFragment.java
+++ gpslogger/src/main/java/com/mendhak/gpslogger/views/GpsSimpleViewFragment.java
@@ -374,8 +374,8 @@ public class GpsSimpleViewFragment extends GenericViewFragment implements View.O
         TextView txtPoints = (TextView) rootView.findViewById(R.id.simpleview_txtPoints);
         TextView txtTravelled = (TextView) rootView.findViewById(R.id.simpleview_txtDistance);
 
-        txtPoints.setText("");
-        txtTravelled.setText("");
+        txtPoints.setText("-");
+        txtTravelled.setText("-");
     }
 
     private String getInterval(long startTime, long endTime) {
