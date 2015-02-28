d6f8cddb12575932ad2273fc6962ce6911f3115f
mendhak <smaltaf@msn.com>
2015-02-28 23:42:29 +0000

    Added cardview for detail view fragment

--- gpslogger/src/main/java/com/mendhak/gpslogger/GpsLoggingService.java
+++ gpslogger/src/main/java/com/mendhak/gpslogger/GpsLoggingService.java
@@ -30,11 +30,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Location;
 import android.location.LocationManager;
-import android.os.Binder;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.IBinder;
-import android.os.SystemClock;
+import android.os.*;
 import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
@@ -490,6 +486,7 @@ public class GpsLoggingService extends Service implements IActionListener {
         if (nfc == null) {
             nfc = new NotificationCompat.Builder(getApplicationContext())
                     .setSmallIcon(R.drawable.gpsloggericon2)
+                    .setPriority(Notification.PRIORITY_MAX)
                     .setContentTitle(getString(R.string.gpslogger_still_running))
                     .setOngoing(true)
                     .setContentIntent(pending)
