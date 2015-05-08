f3eb08bfc696b3b446ccf59dad4032117919e061
mendhak <mendhak@users.noreply.github.com>
2015-05-08 07:03:26 +0100

    Activity recognition off by default

--- gpslogger/src/main/java/com/mendhak/gpslogger/common/Utilities.java
+++ gpslogger/src/main/java/com/mendhak/gpslogger/common/Utilities.java
@@ -289,7 +289,7 @@ public class Utilities {
 
         AppSettings.setGoogleDriveFolderName(prefs.getString("gdocs_foldername", "GPSLogger for Android"));
 
-        AppSettings.setShouldNotLogIfUserIsStill(prefs.getBoolean("activityrecognition_dont_log_if_still", true));
+        AppSettings.setShouldNotLogIfUserIsStill(prefs.getBoolean("activityrecognition_dontlogifstill", false));
     }
 
 
