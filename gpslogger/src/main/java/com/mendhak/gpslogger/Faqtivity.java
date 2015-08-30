4ae6475f4a63b278512914fcf233e3b47556f28d
mendhak <mendhak@users.noreply.github.com>
2015-08-30 12:31:58 +0100

    Basic expandable list view and text view with animation

--- gpslogger/src/main/java/com/mendhak/gpslogger/Faqtivity.java
+++ gpslogger/src/main/java/com/mendhak/gpslogger/Faqtivity.java
@@ -25,11 +25,23 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.ExpandableListView;
+import com.mendhak.gpslogger.views.component.ExpandableListAdapter;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Faqtivity extends ActionBarActivity {
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     WebView browser;
     private org.slf4j.Logger tracer = LoggerFactory.getLogger(Faqtivity.class.getSimpleName());
@@ -52,18 +64,73 @@ public class Faqtivity extends ActionBarActivity {
         }
 
 
-        browser = (WebView) findViewById(R.id.faqwebview);
-        WebSettings settings = browser.getSettings();
-        settings.setLoadWithOverviewMode(true);
-        settings.setUseWideViewPort(true);
-        settings.setBuiltInZoomControls(true);
-        settings.setDisplayZoomControls(false);
-        settings.setJavaScriptEnabled(true);
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
 
-        settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
 
-        browser.loadUrl("http://code.mendhak.com/gpslogger/index.html");
+        // preparing list data
+        prepareListData();
 
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+//
+//        browser = (WebView) findViewById(R.id.faqwebview);
+//        WebSettings settings = browser.getSettings();
+//        settings.setLoadWithOverviewMode(true);
+//        settings.setUseWideViewPort(true);
+//        settings.setBuiltInZoomControls(true);
+//        settings.setDisplayZoomControls(false);
+//        settings.setJavaScriptEnabled(true);
+//
+//        settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+//
+//        browser.loadUrl("http://code.mendhak.com/gpslogger/index.html");
+
+    }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Top 250");
+        listDataHeader.add("Now Showing");
+        listDataHeader.add("Coming Soon..");
+
+        // Adding child data
+        List<String> top250 = new ArrayList<String>();
+        top250.add("<h3>Why isn't it accurate</h3>" +
+                "It all comes down to your hardware, settings and environment. The accuracy is only as good as your phone's GPS chip. Some phones may have 4 meter accuracies, some have 500 meters. Also, using GPS satellites will give you better accuracy but take a longer time; using network location will give worse accuracy but is quicker. You may also want to check your environment, as there can be inaccuracy due to clouds, buildings, sunspots, alien invasion, etc.");
+        top250.add("<h3>How do I remove the notification?</h3>" +
+                "Unfortunately, and annoyingly, the notification needs to stay there. Android 4.x has become very restrictive and will kill foreground services that don't show notifications. There is also no way to work around this as there was in the past. This also means that more applications will start to ask for notification area space, it may get quite crowded soon. At the same time, this is with good reason; the user should know that a foreground service is running and consuming resources, however no means has been provided to allow it to be dismissed.");
+        top250.add("<h3>Why isn't it logging imperial to the file?</h3>" +
+                "The imperial units are only for display purposes and nothing else. When logging, the units are always in SI units - meters and seconds.");
+        top250.add("Pulp Fiction");
+        top250.add("The Good, the Bad and the Ugly");
+        top250.add("The Dark Knight");
+        top250.add("12 Angry Men");
+
+        List<String> nowShowing = new ArrayList<String>();
+        nowShowing.add("The Conjuring");
+        nowShowing.add("Despicable Me 2");
+        nowShowing.add("Turbo");
+        nowShowing.add("Grown Ups 2");
+        nowShowing.add("Red 2");
+        nowShowing.add("The Wolverine");
+
+        List<String> comingSoon = new ArrayList<String>();
+        comingSoon.add("2 Guns");
+        comingSoon.add("The Smurfs 2");
+        comingSoon.add("The Spectacular Now");
+        comingSoon.add("The Canyons");
+        comingSoon.add("Europa Report");
+
+        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), nowShowing);
+        listDataChild.put(listDataHeader.get(2), comingSoon);
     }
 
     @Override
