bf1e2116c609aa420b913c99368e2126a1b00664
mendhak <mendhak@users.noreply.github.com>
2016-03-13 22:11:04 +0000

    Using Apache Common Net smtp client

--- gpslogger/src/main/java/com/mendhak/gpslogger/senders/email/AutoEmailJob.java
+++ gpslogger/src/main/java/com/mendhak/gpslogger/senders/email/AutoEmailJob.java
@@ -1,13 +1,19 @@
 package com.mendhak.gpslogger.senders.email;
 
+import com.mendhak.gpslogger.common.Strings;
 import com.mendhak.gpslogger.common.events.UploadEvents;
 import com.mendhak.gpslogger.common.slf4j.Logs;
 import com.path.android.jobqueue.Job;
 import com.path.android.jobqueue.Params;
 import de.greenrobot.event.EventBus;
+import org.apache.commons.net.smtp.AuthenticatingSMTPClient;
+import org.apache.commons.net.smtp.SMTPReply;
+import org.apache.commons.net.smtp.SimpleSMTPHeader;
 import org.slf4j.Logger;
+import org.apache.commons.net.smtp.SMTPClient;
 
 import java.io.File;
+import java.io.Writer;
 
 
 public class AutoEmailJob extends Job {
@@ -23,7 +29,7 @@ public class AutoEmailJob extends Job {
     String subject;
     String body;
     File[] files;
-    Mail m;
+    //Mail m;
 
 
 
@@ -51,36 +57,98 @@ public class AutoEmailJob extends Job {
 
     @Override
     public void onRun() throws Throwable {
-        m = new Mail(smtpUsername, smtpPassword);
 
-        String[] toArr = csvEmailTargets.split(",");
-        m.setTo(toArr);
+        int port = Strings.toInt(smtpPort,25);
 
-        if (fromAddress != null && fromAddress.length() > 0) {
-            m.setFrom(fromAddress);
-        } else {
-            m.setFrom(smtpUsername);
+        if (!Strings.isNullOrEmpty(fromAddress)) {
+            fromAddress = smtpUsername;
         }
 
-        m.setSubject(subject);
-        m.setBody(body);
+        AuthenticatingSMTPClient client = new AuthenticatingSMTPClient();
+        try {
+            String to = "recipient@email.com";
+            // optionally set a timeout to have a faster feedback on errors
+            client.setDefaultTimeout(10 * 1000);
+            // you connect to the SMTP server
+            client.connect(smtpServer, port);
+            // you say ehlo  and you specify the host you are connecting from, could be anything
+            client.ehlo("localhost");
+            // if your host accepts STARTTLS, we're good everything will be encrypted, otherwise we're done here
+            if (client.execTLS()) {
 
-        m.setPort(smtpPort);
-        m.setSecurePort(smtpPort);
-        m.setSmtpHost(smtpServer);
-        m.setSsl(smtpUseSsl);
+                client.auth(AuthenticatingSMTPClient.AUTH_METHOD.LOGIN, smtpUsername, smtpPassword);
+                checkReply(client);
 
-        for (File f : files) {
-            m.addAttachment(f.getName(), f.getAbsolutePath());
+                client.setSender(fromAddress);
+                checkReply(client);
+
+                client.addRecipient(to);
+                checkReply(client);
+
+                Writer writer = client.sendMessageData();
+
+                if (writer != null) {
+                    SimpleSMTPHeader header = new SimpleSMTPHeader(fromAddress, to, subject);
+                    writer.write(header.toString());
+                    writer.write(body);
+                    writer.close();
+                    if(!client.completePendingCommand()) {// failure
+                        throw new Exception("Failure to send the email "+ client.getReply() + client.getReplyString());
+                    }
+                } else {
+                    throw new Exception("Failure to send the email "+ client.getReply() + client.getReplyString());
+                }
+            } else {
+                throw new Exception("STARTTLS was not accepted "+ client.getReply() + client.getReplyString());
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            client.logout();
+            client.disconnect();
         }
 
-        m.setDebuggable(true);
 
-        LOG.info("Sending email...");
-        if (m.send()) {
-            EventBus.getDefault().post(new UploadEvents.AutoEmail().succeeded());
-        } else {
-            EventBus.getDefault().post(new UploadEvents.AutoEmail().failed());
+
+//        m = new Mail(smtpUsername, smtpPassword);
+//
+//        String[] toArr = csvEmailTargets.split(",");
+//        m.setTo(toArr);
+//
+//        if (fromAddress != null && fromAddress.length() > 0) {
+//            m.setFrom(fromAddress);
+//        } else {
+//            m.setFrom(smtpUsername);
+//        }
+//
+//        m.setSubject(subject);
+//        m.setBody(body);
+//
+//        m.setPort(smtpPort);
+//        m.setSecurePort(smtpPort);
+//        m.setSmtpHost(smtpServer);
+//        m.setSsl(smtpUseSsl);
+//
+//        for (File f : files) {
+//            m.addAttachment(f.getName(), f.getAbsolutePath());
+//        }
+//
+//        m.setDebuggable(true);
+//
+//        LOG.info("Sending email...");
+//        if (m.send()) {
+//            EventBus.getDefault().post(new UploadEvents.AutoEmail().succeeded());
+//        } else {
+//            EventBus.getDefault().post(new UploadEvents.AutoEmail().failed());
+//        }
+    }
+
+
+    private static void checkReply(SMTPClient sc) throws Exception {
+        if (SMTPReply.isNegativeTransient(sc.getReplyCode())) {
+            throw new Exception("Transient SMTP error " + sc.getReply() + sc.getReplyString());
+        } else if (SMTPReply.isNegativePermanent(sc.getReplyCode())) {
+            throw new Exception("Permanent SMTP error " + sc.getReply() + sc.getReplyString());
         }
     }
 
@@ -93,7 +161,7 @@ public class AutoEmailJob extends Job {
     protected boolean shouldReRunOnThrowable(Throwable throwable) {
 
         LOG.error("Could not send email", throwable);
-        EventBus.getDefault().post(new UploadEvents.AutoEmail().failed(m.getSmtpMessages(), throwable));
+        //EventBus.getDefault().post(new UploadEvents.AutoEmail().failed(m.getSmtpMessages(), throwable));
 
         return false;
     }
