d4cbae04c1a0f0b3a4709c296cd9961e0732e20a
Clemens Klug <git@agp8x.org>
2017-06-27 13:20:31 +0200

    add more attributes and tests

--- gpslogger/src/main/java/com/mendhak/gpslogger/loggers/geojson/GeoJSONWriterPoints.java
+++ gpslogger/src/main/java/com/mendhak/gpslogger/loggers/geojson/GeoJSONWriterPoints.java
@@ -8,9 +8,7 @@ import com.mendhak.gpslogger.common.slf4j.Logs;
 
 import org.slf4j.Logger;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.Date;
@@ -28,14 +26,12 @@ public class GeoJSONWriterPoints implements Runnable {
     private final static String TEMPLATE =
             "{\"type\": \"Feature\"," +
                     "\"properties\":{" +
-                    "\"time\":\"%s\"," +
-                    "\"altitude\":\"%s\"" +
                     "%s" +
                     "}," +
                     "\"geometry\":{" +
                     "\"type\":\"Point\",\"coordinates\":" +
                     "%s}}\n";
-    private final static String DESC_TEMPLATE = ",\"description\":\"%s\"";
+    private final static String ATTRIBUTE_TEMPLATE = ",\"%s\":\"%s\"";
     private final static String COORD_TEMPLATE = "[%s,%s]";
     private final static String DIVIDER = ",";
     private String desc;
@@ -51,18 +47,17 @@ public class GeoJSONWriterPoints implements Runnable {
     @Override
     public void run() {
         try {
-            String coords = String.format(COORD_TEMPLATE, location.getLongitude(), location.getLatitude());
 
             synchronized (GeoJSONLogger.lock) {
-                byte[] value = getString(coords, file.exists());
+                byte[] value = getString(file.exists()).getBytes();
 
                 RandomAccessFile raf;
-                if (!file.exists()){
+                if (!file.exists()) {
                     file.createNewFile();
                     raf = new RandomAccessFile(file, "rw");
-                }else {
+                } else {
                     raf = new RandomAccessFile(file, "rw");
-                    raf.seek(file.length()+TRAILER_LENGTH);
+                    raf.seek(file.length() + TRAILER_LENGTH);
                 }
                 raf.write(value);
                 raf.close();
@@ -74,24 +69,47 @@ public class GeoJSONWriterPoints implements Runnable {
 
     }
 
+    /**
+     * Format the GeoJSON entry
+     *
+     * @param append False to prepend the GeoJSON header (initialize file), true to prepend a list dividing character
+     * @return A formatted {@link String} to write at position -TRAILER_LENGTH (or 0 for a new file)
+     */
     @NonNull
-    private byte[] getString(String coords, boolean exists) {
+    protected String getString(boolean append) {
+        String coords = String.format(COORD_TEMPLATE, location.getLongitude(), location.getLatitude());
         StringBuilder value = new StringBuilder();
-        if (exists){
+        if (append) {
             value.append(DIVIDER);
-        }else {
+        } else {
             value.append(HEADER);
         }
         String dateTimeString = Strings.getIsoDateTime(new Date(location.getTime()));
         String extra = "";
+        StringBuilder attributes = new StringBuilder();
+        attributes.append("\"time\":\"").append(dateTimeString).append("\"");
+        attributes.append(String.format(ATTRIBUTE_TEMPLATE, "provider", location.getProvider()));
+        attributes.append(String.format(ATTRIBUTE_TEMPLATE, "time_long", location.getTime()));
         if (desc != null) {
-            extra = String.format(DESC_TEMPLATE, desc);
+            attributes.append(String.format(ATTRIBUTE_TEMPLATE, "description", desc));
         }
+        if (location.hasAccuracy()) {
+            attributes.append(String.format(ATTRIBUTE_TEMPLATE, "accuracy", location.getAccuracy()));
+        }
+        if (location.hasAltitude()) {
+            attributes.append(String.format(ATTRIBUTE_TEMPLATE, "altitude", location.getAltitude()));
+        }
+        if (location.hasBearing()) {
+            attributes.append(String.format(ATTRIBUTE_TEMPLATE, "bearing", location.getBearing()));
+        }
+        if (location.hasSpeed()) {
+            attributes.append(String.format(ATTRIBUTE_TEMPLATE, "speed", location.getSpeed()));
+        }
+
+        extra = attributes.toString();
         value.append(String.format(TEMPLATE,
-                dateTimeString,
-                location.getAltitude(),
                 extra,
                 coords)).append(TRAILER);
-        return value.toString().getBytes();
+        return value.toString();
     }
 }
--- gpslogger/src/test/java/com/mendhak/gpslogger/loggers/geojson/GeoJSONLoggerTest.java
+++ gpslogger/src/test/java/com/mendhak/gpslogger/loggers/geojson/GeoJSONLoggerTest.java
@@ -4,12 +4,14 @@ import android.location.Location;
 
 import com.mendhak.gpslogger.loggers.MockLocations;
 
-import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by clemens on 15.05.17.
@@ -20,23 +22,69 @@ public class GeoJSONLoggerTest {
     @Test
     public void write() throws Exception {
         File file = new File("test.geojson");
-        //file.delete();
-        log = new GeoJSONLogger(file,false);
-        Location loc = MockLocations.builder("MOCK", 12.193, 19.111)
+        log = new GeoJSONLogger(file, false);
+        Location loc = getLocation();
+        log.write(loc);
+        Thread.sleep(1000);
+        assertEquals(0, GeoJSONLogger.getCount());
+        assertTrue(file.exists());
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String line;
+        StringBuilder lines = new StringBuilder();
+        while ((line=br.readLine()) != null){
+            lines.append(line);
+        }
+        file.delete();
+        assertEquals("{\"type\": \"FeatureCollection\",\"features\": [{\"type\": \"Feature\"," +
+                "\"properties\":{\"time\":\"1970-01-01T00:00:00.000Z\",\"provider\":\"MOCK\"," +
+                "\"time_long\":\"0\",\"altitude\":\"9001.0\",\"bearing\":\"91.88\",\"speed\":\"188.44\"}," +
+                "\"geometry\":{\"type\":\"Point\",\"coordinates\":[19.111,12.193]}}]}", lines.toString());
+    }
+
+    private Location getLocation() {
+        return MockLocations.builder("MOCK", 12.193, 19.111)
                 .withAltitude(9001d)
                 .withBearing(91.88f)
                 .withSpeed(188.44f)
                 .build();
-        log.write(loc);
-       // log.write(loc);
-        while (GeoJSONLogger.getCount()>0){}
-        assertEquals(0, GeoJSONLogger.getCount());
-        assertTrue(file.exists());
     }
 
     @Test
     public void annotate() throws Exception {
-        fail("implement me");
+        GeoJSONWriterPoints geojson = new GeoJSONWriterPoints(null, getLocation(), "test", false);
+        String result = geojson.getString(false);
+        String expected = "{\"type\": \"FeatureCollection\",\"features\": [\n" +
+                "{\"type\": \"Feature\",\"properties\":{\"time\":\"1970-01-01T00:00:00.000Z\"," +
+                "\"provider\":\"MOCK\",\"time_long\":\"0\",\"description\":\"test\"" +
+                ",\"altitude\":\"9001.0\",\"bearing\":\"91.88\",\"speed\":\"188.44\"}," +
+                "\"geometry\":{\"type\":\"Point\",\"coordinates\":[19.111,12.193]}}\n]}";
+        assertEquals("annotation", expected, result);
     }
 
+    @Test
+    public void testLocationString() throws Exception {
+        GeoJSONWriterPoints geojson = new GeoJSONWriterPoints(null, getLocation(), null, false);
+        String result = geojson.getString(false);
+        String expected = "{\"type\": \"FeatureCollection\",\"features\": [\n" +
+                "{\"type\": \"Feature\",\"properties\":{\"time\":\"1970-01-01T00:00:00.000Z\"," +
+                "\"provider\":\"MOCK\",\"time_long\":\"0\",\"altitude\":\"9001.0\"," +
+                "\"bearing\":\"91.88\",\"speed\":\"188.44\"}," +
+                "\"geometry\":{\"type\":\"Point\",\"coordinates\":[19.111,12.193]}}\n]}";
+        assertEquals("locationString", expected, result);
+
+
+    }
+    @Test
+    public void testLocationStringAppend() throws Exception {
+        GeoJSONWriterPoints geojson = new GeoJSONWriterPoints(null, getLocation(), null, false);
+        String result = geojson.getString(true);
+        String expected = ",{\"type\": \"Feature\",\"properties\":{\"time\":\"1970-01-01T00:00:00.000Z\"," +
+                "\"provider\":\"MOCK\",\"time_long\":\"0\",\"altitude\":\"9001.0\"," +
+                "\"bearing\":\"91.88\",\"speed\":\"188.44\"}," +
+                "\"geometry\":{\"type\":\"Point\",\"coordinates\":[19.111,12.193]}}\n]}";
+        assertEquals("locationString", expected, result);
+
+
+    }
 }
