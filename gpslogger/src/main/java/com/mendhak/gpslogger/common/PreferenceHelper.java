7945f3c2bbde937f279a97c1cda00f6bd9eb94c4
sekarpdkt <sekarpdkt@gmail.com>
2019-12-26 09:11:33 +0530

    Get and Set method for current user defined activity
    Get and Set method for current user defined activity

--- gpslogger/src/main/java/com/mendhak/gpslogger/common/PreferenceHelper.java
+++ gpslogger/src/main/java/com/mendhak/gpslogger/common/PreferenceHelper.java
@@ -336,6 +336,17 @@ public class PreferenceHelper {
         prefs.edit().putString(PreferenceNames.GOOGLEDRIVE_ACCOUNTNAME, accountName).apply();
     }
 
+    /**
+     * User defined activity, which will be added in plain text logging
+     */
+    @ProfilePreference(name= PreferenceNames.CURRENT_UD_ACTIVITY)
+    public String getCurrentUserDefinedActivity() {
+        return prefs.getString(PreferenceNames.CURRENT_UD_ACTIVITY, "");
+    }
+    
+    public void setCurrentUserDefinedActivity(String val) {
+        prefs.edit().putString(PreferenceNames.CURRENT_UD_ACTIVITY, val).apply();
+    }
 
     /**
      * The minimum seconds interval between logging points
