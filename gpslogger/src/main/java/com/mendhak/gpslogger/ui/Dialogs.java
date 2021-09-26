583ecf40b705c9d68c6c3b143d81c8fe3df461a5
mendhak <mendhak@users.noreply.github.com>
2021-09-26 18:20:56 +0100

    Make them File Folder picker dark as well, if the system is set to dark

--- gpslogger/src/main/java/com/mendhak/gpslogger/ui/Dialogs.java
+++ gpslogger/src/main/java/com/mendhak/gpslogger/ui/Dialogs.java
@@ -21,6 +21,7 @@ package com.mendhak.gpslogger.ui;
 
 
 import android.app.Activity;
+import android.content.res.Configuration;
 
 import androidx.fragment.app.FragmentActivity;
 
@@ -104,11 +105,24 @@ public class Dialogs {
 
 
         StorageChooser.Theme scTheme = new StorageChooser.Theme(activity.getApplicationContext());
-        int[] myScheme = scTheme.getDefaultScheme();
-        myScheme[StorageChooser.Theme.OVERVIEW_HEADER_INDEX] = activity.getResources().getColor(R.color.accentColor);
-        myScheme[StorageChooser.Theme.SEC_ADDRESS_BAR_BG] = activity.getResources().getColor(R.color.accentColor);
-        myScheme[StorageChooser.Theme.SEC_FOLDER_TINT_INDEX] = activity.getResources().getColor(R.color.primaryColor);
-        scTheme.setScheme(myScheme);
+
+        int nightModeFlags = activity.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        switch (nightModeFlags) {
+            case Configuration.UI_MODE_NIGHT_YES:
+            case Configuration.UI_MODE_NIGHT_UNDEFINED:
+                scTheme.setScheme(activity.getResources().getIntArray(com.codekidlabs.storagechooser.R.array.default_dark));
+                break;
+
+            case Configuration.UI_MODE_NIGHT_NO:
+
+                int[] myScheme = scTheme.getDefaultScheme();
+                myScheme[StorageChooser.Theme.OVERVIEW_HEADER_INDEX] = activity.getResources().getColor(R.color.accentColor);
+                myScheme[StorageChooser.Theme.SEC_ADDRESS_BAR_BG] = activity.getResources().getColor(R.color.accentColor);
+                myScheme[StorageChooser.Theme.SEC_FOLDER_TINT_INDEX] = activity.getResources().getColor(R.color.primaryColor);
+                scTheme.setScheme(myScheme);
+                break;
+
+        }
 
         boolean showOverview = Files.hasSDCard(activity);
 
