144f4693813ad0f495457561a6b7063e931f5221
mendhak <mendhak@users.noreply.github.com>
2022-11-09 21:55:16 +0000

    Add input output config

--- assets/generate-pages/.eleventy.js
+++ assets/generate-pages/.eleventy.js
@@ -1,9 +1,9 @@
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 
-module.exports = (function(eleventyConfig){
+module.exports = (function (eleventyConfig) {
     // When generating headings, wrap it in a hyperlink, for easy accessibility. 
-    let markdownLibrary = markdownIt({html: true}).use(markdownItAnchor, { permalink: markdownItAnchor.permalink.headerLink() });
+    let markdownLibrary = markdownIt({ html: true }).use(markdownItAnchor, { permalink: markdownItAnchor.permalink.headerLink() });
     eleventyConfig.setLibrary("md", markdownLibrary);
 
     // Copies the static and image files straight into the output folder, so that the HTML can reference it. 
@@ -13,13 +13,21 @@ module.exports = (function(eleventyConfig){
     //Don't process README.md, that's for me!
     eleventyConfig.ignores.add("README.md");
 
-    eleventyConfig.addCollection('tour', function(collectionApi){
+    eleventyConfig.addCollection('tour', function (collectionApi) {
         return collectionApi.getFilteredByGlob('content/tour*.*');
     });
 
-    eleventyConfig.addCollection('more', function(collectionApi){
+    eleventyConfig.addCollection('more', function (collectionApi) {
         return collectionApi.getFilteredByGlob('content/more*.*');
     });
 
-    
+    // Config values that could be passed via arguments but it's just easier in here. 
+    return {
+        dir: {
+            input: ".",
+            output: "_site",
+            templateFormats: ["html"]
+        }
+    }
+
 });
