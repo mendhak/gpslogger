aefcb3316f9d05eea577a0e7a158d7bbee8473c5
mendhak <mendhak@users.noreply.github.com>
2022-12-30 17:20:17 +0000

    Refactoring and rename

--- gpslogger/src/main/java/com/mendhak/gpslogger/ui/fragments/display/AnnotationViewFragment.java
+++ gpslogger/src/main/java/com/mendhak/gpslogger/ui/fragments/display/AnnotationViewFragment.java
@@ -26,8 +26,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
+
 import com.dd.processbutton.iml.ActionProcessButton;
 import com.mendhak.gpslogger.GpsLoggingService;
 import com.mendhak.gpslogger.R;
@@ -58,50 +60,45 @@ public class AnnotationViewFragment extends GenericViewFragment implements View.
     private final PreferenceHelper preferenceHelper = PreferenceHelper.getInstance();
     private final Session session = Session.getInstance();
 
-    List<ButtonObj> btnList = new ArrayList<>();
-    ButtonObj selectedButton;
+    List<ButtonWrapper> buttonList = new ArrayList<>();
+    ButtonWrapper selectedButton;
 
     @Override
     public void onClick(View v) {
 
     }
 
-    static class ButtonObj {
+    static class ButtonWrapper {
 
         String color;
         String text;
-        ActionProcessButton btn;
+        ActionProcessButton actionButton;
 
-        ButtonObj(ActionProcessButton btn)
-        {
-            this.btn = btn;
-            this.btn.setMode(ActionProcessButton.Mode.ENDLESS);
+        ButtonWrapper(ActionProcessButton actionButton) {
+            this.actionButton = actionButton;
+            this.actionButton.setMode(ActionProcessButton.Mode.ENDLESS);
         }
 
-        String getText()
-        {
+        String getText() {
             return String.valueOf(this.text);
         }
 
-        void setText(String str)
-        {
+        void setText(String str) {
             this.text = str;
-            btn.setText(this.text);
+            this.actionButton.setText(this.text);
         }
 
         public void setColor(String color) {
             this.color = color;
-            if(color == null || "".compareTo(color) == 0){
+            if (color == null || "".compareTo(color) == 0) {
                 setColor("#808080");
-            } else
-            {
-                btn.setBackgroundColor(Color.parseColor(this.color));
+            } else {
+                this.actionButton.setBackgroundColor(Color.parseColor(this.color));
             }
         }
 
-        public  String getColor()
-        {
-            return color;
+        public String getColor() {
+            return this.color;
         }
 
     }
@@ -110,69 +107,60 @@ public class AnnotationViewFragment extends GenericViewFragment implements View.
 
     }
 
-    private void SaveSettings()
-    {
+    private void saveSettings() {
         StringBuilder settings = new StringBuilder("{");
         settings.append("\"buttons\" : [");
-        Iterator<ButtonObj> itr = btnList.iterator();
+        Iterator<ButtonWrapper> itr = buttonList.iterator();
         int idx = 0;
-        while (itr.hasNext())
-        {
-            ButtonObj btn = itr.next();
+        while (itr.hasNext()) {
+            ButtonWrapper btn = itr.next();
             settings.append("{");
             settings.append("\"idx\":").append(idx).append(",");
             settings.append("\"label\":\"").append(btn.getText()).append("\",");
             settings.append("\"color\":\"").append(btn.getColor()).append("\"");
             settings.append("}");
             idx++;
-            if(itr.hasNext())
+            if (itr.hasNext()) {
                 settings.append(",");
+            }
         }
         settings.append("]");
         settings.append("}");
         preferenceHelper.setAnnotationButtonSettings(settings.toString());
     }
 
-    private void LoadSettings()
-    {
+    private void loadSettings() {
         String settings = preferenceHelper.getAnnotationButtonSettings();
-        if(settings == null || "".compareTo(settings) == 0)
+        if (settings == null || "".compareTo(settings) == 0) {
             return;
+        }
 
         try {
             JSONObject settingsObject = new JSONObject(settings);
             JSONArray buttonArrays = settingsObject.getJSONArray("buttons");
-            for(int i = 0; i < buttonArrays.length() && i < btnList.size(); ++i)
-            {
+            for (int i = 0; i < buttonArrays.length() && i < buttonList.size(); ++i) {
                 JSONObject btnObj = buttonArrays.getJSONObject(i);
                 int idx = btnObj.getInt("idx");
                 String txt = btnObj.getString("label");
                 String color = btnObj.getString("color");
 
-                if(idx < 0 || idx >= btnList.size())
-                {
+                if (idx < 0 || idx >= buttonList.size()) {
                     LOG.error("Wrong index in settings: " + idx);
                     continue;
                 }
 
-                ButtonObj btn = btnList.get(idx);
+                ButtonWrapper btn = buttonList.get(idx);
                 btn.setText(txt);
                 btn.setColor(color);
             }
 
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             LOG.error("Exception loading annotation settings: " + e.getMessage(), e);
         }
     }
 
     public static AnnotationViewFragment newInstance() {
-
         AnnotationViewFragment fragment = new AnnotationViewFragment();
-        Bundle bundle = new Bundle(1);
-        bundle.putInt("a_number", 1);
-
-        fragment.setArguments(bundle);
         return fragment;
     }
 
@@ -183,26 +171,27 @@ public class AnnotationViewFragment extends GenericViewFragment implements View.
 
     @Override
     public boolean onResult(@NonNull String dialogTag, int which, @NonNull Bundle extras) {
-        if(which == BUTTON_POSITIVE) {
+        if (which == BUTTON_POSITIVE) {
 
             Integer idx = null;
             try {
                 idx = Integer.valueOf(dialogTag.substring(3));
-            } catch (NumberFormatException ignored)
-            {
             }
-            if(idx == null || idx < 0 || idx >= btnList.size())
-            {
+            catch (NumberFormatException ignored) {
+
+            }
+
+            if (idx == null || idx < 0 || idx >= buttonList.size()) {
                 LOG.error("Could not find button " + dialogTag);
                 return true;
             }
 
-            ButtonObj btn = btnList.get(idx);
+            ButtonWrapper btn = buttonList.get(idx);
             String enteredText = extras.getString("annotations");
             String color = extras.getString("color");
             btn.setText(enteredText);
             btn.setColor(color);
-            SaveSettings();
+            saveSettings();
             return true;
         }
         return false;
@@ -219,23 +208,23 @@ public class AnnotationViewFragment extends GenericViewFragment implements View.
 
         }
 
-        btnList.add(new ButtonObj(rootView.findViewById(R.id.an_b11)));
-        btnList.add(new ButtonObj(rootView.findViewById(R.id.an_b12)));
-        btnList.add(new ButtonObj(rootView.findViewById(R.id.an_b13)));
-        btnList.add(new ButtonObj(rootView.findViewById(R.id.an_b21)));
-        btnList.add(new ButtonObj(rootView.findViewById(R.id.an_b22)));
-        btnList.add(new ButtonObj(rootView.findViewById(R.id.an_b23)));
-        btnList.add(new ButtonObj(rootView.findViewById(R.id.an_b31)));
-        btnList.add(new ButtonObj(rootView.findViewById(R.id.an_b32)));
-        btnList.add(new ButtonObj(rootView.findViewById(R.id.an_b33)));
+        buttonList.add(new ButtonWrapper(rootView.findViewById(R.id.an_b11)));
+        buttonList.add(new ButtonWrapper(rootView.findViewById(R.id.an_b12)));
+        buttonList.add(new ButtonWrapper(rootView.findViewById(R.id.an_b13)));
+        buttonList.add(new ButtonWrapper(rootView.findViewById(R.id.an_b21)));
+        buttonList.add(new ButtonWrapper(rootView.findViewById(R.id.an_b22)));
+        buttonList.add(new ButtonWrapper(rootView.findViewById(R.id.an_b23)));
+        buttonList.add(new ButtonWrapper(rootView.findViewById(R.id.an_b31)));
+        buttonList.add(new ButtonWrapper(rootView.findViewById(R.id.an_b32)));
+        buttonList.add(new ButtonWrapper(rootView.findViewById(R.id.an_b33)));
 
         int i = 0;
         AnnotationViewFragment fragment = this;
-        for( ButtonObj btn : btnList) {
-            btn.setText("Annotation " + i);
-            btn.setColor("#808080");
+        for (ButtonWrapper buttonWrapper : buttonList) {
+            buttonWrapper.setText("Annotation " + i);
+            buttonWrapper.setColor("#808080");
             final int btnIdx = i;
-            btn.btn.setOnLongClickListener(new View.OnLongClickListener() {
+            buttonWrapper.actionButton.setOnLongClickListener(new View.OnLongClickListener() {
                 @Override
                 public boolean onLongClick(View v) {
                     SimpleFormDialog.build()
@@ -244,52 +233,50 @@ public class AnnotationViewFragment extends GenericViewFragment implements View.
                             .fields(
                                     Input.plain("annotations")
                                             .hint(R.string.annotation_value)
-                                            .text(String.valueOf(btn.getText())),
+                                            .text(String.valueOf(buttonWrapper.getText())),
                                     Input.plain("color")
-                                            .text(String.valueOf(btn.getColor()))
+                                            .text(String.valueOf(buttonWrapper.getColor()))
                                             .hint(R.string.annotation_color)
                                             .validatePattern("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$",
-                                                             R.string.annotation_color_error)
+                                                    R.string.annotation_color_error)
                             ).show(fragment, "btn" + btnIdx);
                     return true;
                 }
             });
-            btn.btn.setOnClickListener((b) -> onBtnClick(btn));
+            buttonWrapper.actionButton.setOnClickListener((b) -> onBtnClick(buttonWrapper));
             i++;
 
         }
-        LoadSettings();
+        loadSettings();
 
         return rootView;
     }
 
-    private void onBtnClick(ButtonObj  btn) {
-        LOG.info("Notification Annotation entered : " + btn.getText());
-        selectedButton = btn;
+    private void onBtnClick(ButtonWrapper wrapper) {
+        LOG.info("Notification Annotation entered : " + wrapper.getText());
+        selectedButton = wrapper;
         Intent serviceIntent = new Intent(getContext(), GpsLoggingService.class);
-        serviceIntent.putExtra(IntentConstants.SET_DESCRIPTION, btn.getText());
-        ContextCompat.startForegroundService(getContext(),  serviceIntent);
+        serviceIntent.putExtra(IntentConstants.SET_DESCRIPTION, wrapper.getText());
+        ContextCompat.startForegroundService(getContext(), serviceIntent);
     }
 
-    void updateButtons(boolean enable)
-    {
-        selectedButton.btn.setProgress(enable ? 0 : 50);
+    void updateButtons(boolean enable) {
+        selectedButton.actionButton.setProgress(enable ? 0 : 50);
 
-        for(ButtonObj btnObj : btnList)
-        {
-            btnObj.btn.setEnabled(enable);
+        for (ButtonWrapper btnObj : buttonList) {
+            btnObj.actionButton.setEnabled(enable);
             btnObj.setText(btnObj.getText());
             btnObj.setColor(btnObj.getColor());
         }
     }
 
     @EventBusHook
-    public void onEventMainThread(ServiceEvents.WaitingForLocation waitingForLocation){
+    public void onEventMainThread(ServiceEvents.WaitingForLocation waitingForLocation) {
         updateButtons(!waitingForLocation.waiting);
     }
 
     @EventBusHook
-    public void onEventMainThread(ServiceEvents.LoggingStatus loggingStatus){
+    public void onEventMainThread(ServiceEvents.LoggingStatus loggingStatus) {
         //updateButtons(loggingStatus.loggingStarted);
     }
 }
