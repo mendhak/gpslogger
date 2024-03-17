a317cee1d3a64ba6d86735af8d7a4768383494d5
mendhak <mendhak@users.noreply.github.com>
2024-03-17 15:20:52 +0000

    Openstreetmap WorkManager

--- gpslogger/src/main/java/com/mendhak/gpslogger/senders/osm/OpenStreetMapManager.java
+++ gpslogger/src/main/java/com/mendhak/gpslogger/senders/osm/OpenStreetMapManager.java
@@ -22,12 +22,13 @@ package com.mendhak.gpslogger.senders.osm;
 import android.content.Context;
 import android.net.Uri;
 
-import com.birbit.android.jobqueue.CancelResult;
-import com.birbit.android.jobqueue.JobManager;
-import com.birbit.android.jobqueue.TagConstraint;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+
 import com.mendhak.gpslogger.common.AppSettings;
 import com.mendhak.gpslogger.common.PreferenceHelper;
 import com.mendhak.gpslogger.common.Strings;
+import com.mendhak.gpslogger.common.Systems;
 import com.mendhak.gpslogger.common.slf4j.Logs;
 import com.mendhak.gpslogger.senders.FileSender;
 
@@ -39,7 +40,9 @@ import net.openid.appauth.AuthorizationServiceConfiguration;
 import org.json.JSONException;
 import org.slf4j.Logger;
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 public class OpenStreetMapManager extends FileSender {
 
@@ -47,7 +50,6 @@ public class OpenStreetMapManager extends FileSender {
 
     private static final Logger LOG = Logs.of(OpenStreetMapManager.class);
 
-    static final String OSM_GPSTRACE_URL = "https://www.openstreetmap.org/api/0.6/gpx/create";
     private PreferenceHelper preferenceHelper;
 
     public OpenStreetMapManager(PreferenceHelper preferenceHelper) {
@@ -128,21 +130,15 @@ public class OpenStreetMapManager extends FileSender {
     public void uploadFile(String fileName) {
         File gpxFolder = new File(preferenceHelper.getGpsLoggerFolder());
         final File chosenFile = new File(gpxFolder, fileName);
+        String tag = String.valueOf(Objects.hashCode(chosenFile));
 
-        final String gpsTraceUrl = OSM_GPSTRACE_URL;
+        HashMap<String, Object> dataMap = new HashMap<String, Object>() {{
+            put("filePath", chosenFile.getAbsolutePath());
+        }};
 
-
-        final String description = preferenceHelper.getOSMDescription();
-        final String tags = preferenceHelper.getOSMTags();
-        final String visibility = preferenceHelper.getOSMVisibility();
-
-        final JobManager jobManager = AppSettings.getJobManager();
-        jobManager.cancelJobsInBackground(new CancelResult.AsyncCancelCallback() {
-            @Override
-            public void onCancelled(CancelResult cancelResult) {
-                jobManager.addJobInBackground(new OSMJob(gpsTraceUrl, chosenFile, description, tags, visibility));
-            }
-        }, TagConstraint.ANY, OSMJob.getJobTag(chosenFile));
+        OneTimeWorkRequest workRequest = Systems.getBasicOneTimeWorkRequest(OpenStreetMapWorker.class, dataMap);
+        WorkManager.getInstance(AppSettings.getInstance())
+                .enqueueUniqueWork(tag, androidx.work.ExistingWorkPolicy.REPLACE, workRequest);
 
     }
 
