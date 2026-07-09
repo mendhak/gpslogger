9c67752a0aa1679c074d141d923007e215b6fd4b
mendhak <mendhak@users.noreply.github.com>
2026-07-09 21:20:27 +0100

    Revert "Do not display passwords in plaintext"
    This reverts commit 345e992b75c97bd2ed10af55f3c07ef5b0c95ac5.

--- gpslogger/src/main/java/com/mendhak/gpslogger/common/PreferenceHelper.java
+++ gpslogger/src/main/java/com/mendhak/gpslogger/common/PreferenceHelper.java
@@ -677,6 +677,7 @@ public class PreferenceHelper {
         prefs.edit().putString(PreferenceNames.LOG_TO_URL_BASICAUTH_USERNAME, username).apply();
     }
 
+    @ProfilePreference(name=PreferenceNames.LOG_TO_URL_BASICAUTH_PASSWORD)
     public String getCustomLoggingBasicAuthPassword() {
         return prefs.getString(PreferenceNames.LOG_TO_URL_BASICAUTH_PASSWORD, "");
     }
